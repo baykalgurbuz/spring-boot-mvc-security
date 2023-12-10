9ee01909ea8f5ba610b8dc572586f6fd4808752e
baykalgurbuz <ebaykalgurbuz@gmail.com>
2023-12-10 14:22:17 +0300

    first commit

---
+++ src/main/java/com/mycoolapp/springbootmvcsecuirty/SpringBootMvcSecuirtyApplication.java
@@ -0,0 +1,13 @@
+package com.mycoolapp.springbootmvcsecuirty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcSecuirtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcSecuirtyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycoolapp/springbootmvcsecuirty/SpringBootMvcSecuirtyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycoolapp.springbootmvcsecuirty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcSecuirtyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
