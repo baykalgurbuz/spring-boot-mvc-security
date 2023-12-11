78bad9062598bf9b7ac8c3e8c5856da73b1cf4ab
baykalgurbuz <ebaykalgurbuz@gmail.com>
2023-12-11 17:51:03 +0300

    Working with db for mvc security

--- src/main/java/com/mycoolapp/springbootmvcsecuirty/security/DemoSecurityConfig.java
+++ src/main/java/com/mycoolapp/springbootmvcsecuirty/security/DemoSecurityConfig.java
@@ -6,11 +6,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
-    @Bean
+   /*
+   @Bean
     public InMemoryUserDetailsManager userDetailsManager()
     {
         UserDetails john = User.builder()
@@ -29,6 +34,15 @@ public class DemoSecurityConfig {
                 .roles("EMPLOYEE","MANAGER","ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(john,mary,susan);
+    }*/
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource)
+    {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        //Custom table configuration
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id,pw,active from members where user_id=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id,role from roles where user_id=?");
+        return jdbcUserDetailsManager;
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
